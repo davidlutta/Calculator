e92ff2ccbdb6fb12bbb9d49ae7227a7ef27fa1c7
davidlutta <davidlutta2010@gmail.com>
2018-03-20 16:49:37 +0300

    added HTML and Scripts

---
+++ js/scripts.js
@@ -0,0 +1,23 @@
+var add = function (number1, number2) { return number1 + number2; };
+
+var number1 = parseInt(prompt("Give me a Number to add for you mate"));
+var number2 = parseInt(prompt("Just one more"));
+
+var result = add(number1, number2);
+
+alert(result);
+
+var Subtract = function (number1, number2) { return number1 - number2; };
+
+var number1 = parseInt(prompt("Give me a Number to Subtract for you mate"));
+var number2 = parseInt(prompt("Just one more"));
+var result = Subtract(number1, number2);
+
+alert(result);
+
+var divide = function (number1, number2) { return number1 / number2; };
+
+var number1 = parseInt(prompt("Give me a Number to divide for you mate"));
+var number2 = parseInt(prompt("Just one more"));
+var result = divide(number1, number2);
+alert(result);
